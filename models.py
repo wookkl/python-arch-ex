ba14eda92a6c7877aad4198d61427218b56457be
wookkl <wjddnr315@gmail.com>
2022-01-17 22:45:52 +0900

    Add domain model and test code

---
+++ models.py
@@ -0,0 +1,43 @@
+from datetime import date
+from typing import Optional
+
+from pydantic.dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    order_id:str
+    sku: str
+    qty: int
+
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qty:int, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations = set()
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self._purchased_quantity >= line.qty
+
+
+
+
